81e4d05b0c6569a9c8e79bc585011d4ce88a54f5
Bartolini515 <bartolini0690@gmail.com>
2026-04-06 13:32:40 +0200

    Obejście mysqlclient

--- backend/forumSamorzadu/wsgi.py
+++ backend/forumSamorzadu/wsgi.py
@@ -7,6 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+# Obejście mysqlclient przy użyciu pymysql
+import pymysql
+pymysql.version_info = (2, 2, 1, 'final', 0)
+pymysql.install_as_MySQLdb()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
